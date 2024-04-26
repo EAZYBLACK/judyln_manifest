56c1a58384a12fb911d4e86395114a6e027e2f45
EAZYBLACK <68810731+EAZYBLACK@users.noreply.github.com>
2024-04-26 12:36:21 +0300

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -19,37 +19,9 @@ init_ksu() {
   cd ./../../../
 }
 
-# Main script
-
-echo "Select the device:"
-echo "1. judyln"
-echo "2. judypn"
-echo "3. judyp"
-echo "4. ALL"
-
-read -p "Enter your choice (1-4): " device_choice
-
-case ${device_choice} in
-  1)
-    device="judyln"
-    ;;
-  2)
-    device="judypn"
-    ;;
-  3)
-    device="judyp"
-    ;;
-  4)
-    device_arr=("judyln" "judyp" "judypn")
-    ;;
-  *)
-    echo "Invalid choice. Exiting..."
-    exit 1
-    ;;
-esac
-
-echo "Rom Bringup"
-read -p "New rom prefix: " rom
+device_choice=4
+device_arr=("judyln" "judyp" "judypn")
+rom=lineage
 
 if [ "${device_choice}" == 4 ]; then
   for ((i = 0; i < ${#device_arr[@]}; i++)); do
